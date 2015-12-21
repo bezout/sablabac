17d5b67e08b181ab600dcd5b80eb0728ec05c27a
datta <datta.ramadasan@gmail.com>
2015-12-21 21:07:34 +0100

    simple_lma.cpp

--- simple_lma.cpp
+++ simple_lma.cpp
@@ -4,26 +4,9 @@
 
 struct clement_type{}; void clement(clement_type){}
 
-struct Parameters{ double x,y;};
-
-struct Reprojection
-{
-  bool operator()(const Parameters& p, double& r) const
-  {
-    r = p.x * p.x + 2.0 * p.y;
-    return true;
-  }
-  void analytical(const Parameters& p, Eigen::Matrix<double,1,2>& mat) const
-  {
-    mat(0) = 2.0*p.x;
-    mat(1) = 2.0;
-  }
-};
+struct Adl{};
 
 template<class> struct Type{};
-
-constexpr size_t size(Type<Parameters>) { return 2; }
-constexpr size_t size(Type<double>) { return 1; }
 
 template<class> struct AnalyseExtractFromType;
 template<class Return, class F, class P, class R> struct AnalyseExtractFromType< Return (F::*) (const P&, R&) const >
@@ -54,9 +37,20 @@ struct NormalEquation
   Residual e;
   Delta delta;
   
+  void compute_jacobian(const auto& bundle)
+  {
+    bundle.call_analytical(j);
+  }
+
+  std::pair<Float,bool> compute_error(const auto& bundle)
+  {
+    bool success = bundle.f(*bundle.parameters,e[0]);
+    return {e.squaredNorm(),success};
+  }
+  
   Delta& solve()
   {
-    return delta = (j.transpose() * j + Hessian::Identity()*0.1).llt().solve(-j.transpose()*e);
+    return delta = (j.transpose() * j + Hessian::Identity()*0.001).llt().solve(-j.transpose()*e);
   }
 };
 
@@ -64,20 +58,19 @@ template<class Parameters, class F> struct Bundle0
 {
   Parameters* parameters = nullptr;
   F f;
+  
+  void call_analytical(auto& j) const
+  {
+    f.analytical(*parameters,j);
+  }
 };
-
-void update_parameters(Parameters& parameters, const Eigen::Matrix<double,2,1>& delta)
-{
-  parameters.x += delta[0];
-  parameters.y += delta[1];
-}
 
 template<class F> struct Solver
 {
   using InfoFunctor = AnalyseFunctor<F>;
-  using Parameters = typename InfoFunctor::Parameters;
-  using Residual = typename InfoFunctor::Residual;
-  using Bundle = Bundle0<Parameters,F>;
+  using Parameters  = typename InfoFunctor::Parameters;
+  using Residual    = typename InfoFunctor::Residual;
+  using Bundle      = Bundle0<Parameters,F>;
   
   Bundle bundle;
   
@@ -91,34 +84,56 @@ template<class F> struct Solver
   Solver& solve(Type<Float>)
   {
     NormalEquation<Float,size(Type<Parameters>{}),size(Type<Residual>{})> normal_equation;
-    std::pair<double,bool> pair = compute_error(bundle, normal_equation.e);
-    std::cout << " erreur : " << pair.first << std::endl;
+    std::pair<Float,bool> pair = normal_equation.compute_error(bundle);
+    std::cout << " erreur initial : " << pair.first << std::endl;
     
-    compute_jacobian(bundle,normal_equation.j);
-    std::cout << " jacobian : " << normal_equation.j << std::endl;
-    //auto& delta = normal_equation.solve().transpose();
-    update_parameters(*bundle.parameters,normal_equation.solve());
-    
-    pair = compute_error(bundle, normal_equation.e);
-    std::cout << " erreur : " << pair.first << std::endl;
+    do
+    {
+      normal_equation.compute_jacobian(bundle);
+
+      auto& delta = normal_equation.solve();
+      update_parameters(*bundle.parameters,delta.data());
+      
+      pair = normal_equation.compute_error(bundle);
+      std::cout << " erreur courante : " << pair.first << std::endl;
+    } while(!stop());
     
     return *this;
   }
   
-  std::pair<double,bool> compute_error(const Bundle& bundle, auto& e)
+  bool stop() { return true; }  
+};
+
+
+struct Parameters{ double x,y;};
+
+struct Reprojection
+{
+  bool operator()(const Parameters& p, double& r) const
   {
-    bool success = bundle.f(*bundle.parameters,e[0]);
-    return {e.squaredNorm(),success};
+    r = p.x * p.x + 2.0 * p.y;
+    return true;
   }
-  
-  void compute_jacobian(const Bundle& bundle, auto& j)
+
+  void analytical(const Parameters& p, auto& mat) const
   {
-    bundle.f.analytical(*bundle.parameters,j);
+    mat[0] = 2.0*p.x;
+    mat[1] = 2.0;
   }
 };
+
+constexpr size_t size(Type<Parameters>) { return 2; }
+constexpr size_t size(Type<double>) { return 1; }
+
+
+void update_parameters(Parameters& parameters, const double delta[2])
+{
+  parameters.x += delta[0];
+  parameters.y += delta[1];
+}
 
 int main()
 {
   Parameters parameters {1,1};
-  Solver<Reprojection>{}.add(Reprojection{},&parameters).solve(Type<double>{}).solve(Type<double>{});
+  Solver<Reprojection>{}.add(Reprojection{},&parameters).solve(Type<double>{});
 }
